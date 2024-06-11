47d0fca83140e1b6a04b519c9efc5af640d7bd58
Shoomsol <154993685+Shoomsol@users.noreply.github.com>
2024-06-11 12:14:42 +0100

    abstracted states between adjustments into functions

--- modelisation/hyloModelisation.py
+++ modelisation/hyloModelisation.py
@@ -12,14 +12,14 @@ class Action(enum.Enum):
     BurnFSOL = 2
     MintXSOL = 3
     BurnXSOL = 4
-    StabilityPoolAdjustment = 5
-    StabilityPool2Adjustment = 6
+    StabilityPoolSOL = 5
+    StabilityPoolxSOL = 6
     UpdateMarketPrices = 7
     UpdatefSOLInStabilityPool = 8
 
 
 # Using namedtuple to define a Simulation State
-SimulationState = namedtuple('SimulationState', 'nSOL pF pX nF nX stab_nF stab2_nF')
+SimulationState = namedtuple('SimulationState', 'nSOL pF pX nF nX stabSOL_nF stabxSOL_nF')
 
 class Simulation:
     def __init__(self, config_path: str = 'config.ini') -> None:
@@ -41,9 +41,9 @@ class Simulation:
     def initialize_simulation(self, nSOL: float, pSOL_initial: float) -> SimulationState:
         nF = (pSOL_initial * nSOL) / 2
         nX = nF
-        stab_nF = nF * self.fSOL_staked_per
-        stab2_nF = nF * self.fSOL_staked_per_2
-        return SimulationState(nSOL=nSOL, pF=1, pX=1, nF=nF, nX=nX, stab_nF=stab_nF, stab2_nF=stab2_nF)
+        stabSOL_nF = nF * self.fSOL_staked_per
+        stabxSOL_nF = nF * self.fSOL_staked_per_2
+        return SimulationState(nSOL=nSOL, pF=1, pX=1, nF=nF, nX=nX, stabSOL_nF=stabSOL_nF, stabxSOL_nF=stabxSOL_nF)
 
     def handle_action(
         self, 
@@ -64,19 +64,19 @@ class Simulation:
             new_nX, new_nSOL = mint_xSOL(state.nX, state.nSOL, sign * amount * state.nX, state.pX, pSOL_current)
             return state._replace(nX=new_nX, nSOL=new_nSOL), False
         
-        elif action == Action.StabilityPoolAdjustment:
-            adjustment, changed = use_stability_pool(state.nF, state.stab_nF, amount, state.nX, state.pX, state.pF)
+        elif action == Action.StabilityPoolSOL:
+            adjustment, changed = use_stability_pool(state.nF, state.stabSOL_nF, amount, state.nX, state.pX, state.pF)
             new_nF, new_nSOL = mint_fSOL(state.nF, state.nSOL, adjustment, state.pF, pSOL_current)
-            new_stab_nF = state.stab_nF - (state.nF - new_nF)
+            new_stabSOL_nF = state.stabSOL_nF - (state.nF - new_nF)
 
-            return state._replace(nF=new_nF, nSOL=new_nSOL, stab_nF=new_stab_nF), changed
+            return state._replace(nF=new_nF, nSOL=new_nSOL, stabSOL_nF=new_stabSOL_nF), changed
         
-        elif action == Action.StabilityPool2Adjustment:
-            adjustment_nF, adjustment_nX, changed = use_stability_pool_2(state.nSOL, state.nF, state.stab2_nF, amount, state.nX, state.pX, state.pF, pSOL_current)
+        elif action == Action.StabilityPoolxSOL:
+            adjustment_nF, adjustment_nX, changed = use_stability_pool_2(state.nSOL, state.nF, state.stabxSOL_nF, amount, state.nX, state.pX, state.pF, pSOL_current)
             new_nF, new_nSOL_1 = mint_fSOL(state.nF, state.nSOL, adjustment_nF, state.pF, pSOL_current)
             new_nX, new_nSOL_2 = mint_xSOL(state.nX, new_nSOL_1, adjustment_nX, state.pX, pSOL_current)
-            new_stab2_nF = state.stab2_nF - (state.nF - new_nF)
-            return state._replace(nF=new_nF, nX=new_nX, nSOL=new_nSOL_2, stab2_nF=new_stab2_nF), changed
+            new_stabxSOL_nF = state.stabxSOL_nF - (state.nF - new_nF)
+            return state._replace(nF=new_nF, nX=new_nX, nSOL=new_nSOL_2, stabxSOL_nF=new_stabxSOL_nF), changed
         
         elif action == Action.UpdateMarketPrices:
             new_pX = recalculate_pX(state.nSOL, pSOL_current, state.nF, state.pF, state.nX)
@@ -84,15 +84,54 @@ class Simulation:
         
         elif action == Action.UpdatefSOLInStabilityPool:
             if amount > stab_mod_fSOL_SOL:
-                new_stab_nF = update_fSOL_in_stability_pool(state.stab_nF, state.nF, self.min_recovery_per, self.max_recovery_per, self.fSOL_staked_per)
+                new_stabSOL_nF = update_fSOL_in_stability_pool(state.stabSOL_nF, state.nF, self.min_recovery_per, self.max_recovery_per, self.fSOL_staked_per)
             else:
-                new_stab_nF = state.stab_nF
+                new_stabSOL_nF = state.stabSOL_nF
             if amount > stab_mod_fSOL_xSOL:
-                new_stab2_nF = update_fSOL_in_stability_pool(state.stab2_nF, state.nF, self.min_recovery_per_2, self.max_recovery_per_2, self.fSOL_staked_per_2)
+                new_stabxSOL_nF = update_fSOL_in_stability_pool(state.stabxSOL_nF, state.nF, self.min_recovery_per_2, self.max_recovery_per_2, self.fSOL_staked_per_2)
             else:
-                new_stab2_nF = state.stab2_nF
-            return state._replace(stab_nF=new_stab_nF, stab2_nF=new_stab2_nF), False
+                new_stabxSOL_nF = state.stabxSOL_nF
+            return state._replace(stabSOL_nF=new_stabSOL_nF, stabxSOL_nF=new_stabxSOL_nF), False
+        
+    
+    def update_stability_pools(self, state, pSOL_current, stab_mod_fSOL_xSOL, stab_mod_fSOL_SOL, collateral_ratio):
+        pre_UpdatefSOLInStabilityPool_values = {
+            "stabSOL_nF": state.stabSOL_nF,
+            "stabxSOL_nF": state.stabxSOL_nF
+        }
 
+        # Update fSOL in stability pools
+        state, _ = self.handle_action(state, Action.UpdatefSOLInStabilityPool, collateral_ratio, pSOL_current, stab_mod_fSOL_xSOL, stab_mod_fSOL_SOL)
+
+        post_UpdatefSOLInStabilityPool_values = {
+            "stabSOL_nF": state.stabSOL_nF,
+            "stabxSOL_nF": state.stabxSOL_nF
+        }
+
+        pre_StabilityPoolxSOL_values = {
+            "fSOL_xSOL": (state.nSOL, state.nF, state.nX, state.pX),
+            "stabxSOL_nF": state.stabxSOL_nF
+        }
+        state, stability_pool_fSOL_xSOL_changed = self.handle_action(state, Action.StabilityPoolxSOL, stab_mod_fSOL_xSOL, pSOL_current)
+        post_StabilityPoolxSOL_values = {
+            "fSOL_xSOL": (state.nSOL, state.nF, state.nX, state.pX),
+            "stabxSOL_nF": state.stabxSOL_nF
+        }
+        collateral_ratio_post_stab_xSOL = calculate_collateral_ratio(state.nSOL, pSOL_current, state.nF, state.pF)
+
+        pre_StabilityPoolSOL_values = {
+            "fSOL_xSOL": (state.nSOL, state.nF, state.nX, state.pX),
+            "stabSOL_nF": state.stabSOL_nF
+        }
+        state, stability_pool_fSOL_SOL_changed = self.handle_action(state, Action.StabilityPoolSOL, stab_mod_fSOL_SOL, pSOL_current)
+        post_StabilityPoolSOL_values = {
+            "fSOL_xSOL": (state.nSOL, state.nF, state.nX, state.pX),
+            "stabSOL_nF": state.stabSOL_nF
+        }
+        # Calculate collateral ratios post stability pool adjustments
+        collateral_ratio_post_stab_SOL = calculate_collateral_ratio(state.nSOL, pSOL_current, state.nF, state.pF)
+
+        return state, stability_pool_fSOL_xSOL_changed, stability_pool_fSOL_SOL_changed, pre_UpdatefSOLInStabilityPool_values, post_UpdatefSOLInStabilityPool_values, collateral_ratio_post_stab_xSOL, pre_StabilityPoolxSOL_values, post_StabilityPoolxSOL_values, pre_StabilityPoolSOL_values, post_StabilityPoolSOL_values, collateral_ratio_post_stab_SOL
 
     def run_simulation(
         self, 
@@ -126,38 +165,13 @@ class Simulation:
                 state, _ = self.handle_action(state, action, amount, pSOL_current)
 
             
-            pre_stab_nF = state.stab_nF
-            pre_stab2_nF = state.stab2_nF
+            state, stability_pool_fSOL_xSOL_changed, stability_pool_fSOL_SOL_changed, pre_UpdatefSOLInStabilityPool_values, post_UpdatefSOLInStabilityPool_values, collateral_ratio_post_stab_xSOL, pre_StabilityPoolxSOL_values, post_StabilityPoolxSOL_values, pre_StabilityPoolSOL_values, post_StabilityPoolSOL_values, collateral_ratio_post_stab_SOL = self.update_stability_pools(
+                state, pSOL_current, stab_mod_fSOL_xSOL, stab_mod_fSOL_SOL, collateral_ratio
+            )
 
-            state, _ = self.handle_action(state, Action.UpdatefSOLInStabilityPool, collateral_ratio , pSOL_current, stab_mod_fSOL_xSOL, stab_mod_fSOL_SOL)
-
-            post_stab_nF = state.stab_nF
-            post_stab2_nF = state.stab2_nF
-            
-
-            #Used for tracking between each step, pre usage of stability pool fSOL_xSOL 
-            pre_fSOL_xSOL_nSOL = state.nSOL
-            pre_fSOL_xSOL_nF = state.nF
-            pre_fSOL_xSOL_nX = state.nX
-            pre_fSOL_xSOL_pX = state.pX
-
-
-            state, stability_pool_fSOL_xSOL_changed = self.handle_action(state, Action.StabilityPool2Adjustment, stab_mod_fSOL_xSOL, pSOL_current)
-
-            #Used for tracking between each step, pre usage of stability pool fSOL_SOL
-            pre_fSOL_SOL_nSOL = state.nSOL
-            pre_fSOL_SOL_nF = state.nF
-            pre_fSOL_SOL_nX = state.nX
-            pre_fSOL_SOL_pX = state.pX
-
-            collateral_ratio_post_1 = calculate_collateral_ratio(state.nSOL, pSOL_current, state.nF, state.pF)
-            
-            state, stability_pool_fSOL_SOL_changed = self.handle_action(state, Action.StabilityPoolAdjustment, stab_mod_fSOL_SOL, pSOL_current)
-
-            collateral_ratio_post_2 = calculate_collateral_ratio(state.nSOL, pSOL_current, state.nF, state.pF)
-
-            stability_pool_fSOL_xSOL_usage_nF_redeemed = pre_fSOL_xSOL_nF - pre_fSOL_SOL_nF
-            stability_pool_fSOL_SOL_usage_nF_redeemed = pre_fSOL_SOL_nF - state.nF
+            # Calculate stability pool usage
+            stability_pool_fSOL_xSOL_usage_nF_redeemed = pre_StabilityPoolxSOL_values['fSOL_xSOL'][1] - post_StabilityPoolxSOL_values['fSOL_xSOL'][1]
+            stability_pool_fSOL_SOL_usage_nF_redeemed = pre_StabilityPoolSOL_values['fSOL_xSOL'][1] - post_StabilityPoolSOL_values['fSOL_xSOL'][1]
 
             if stability_pool_fSOL_xSOL_changed:
                 stability_pool_fSOL_xSOL_non_zero_count += 1
@@ -170,36 +184,36 @@ class Simulation:
             day_data = {
                 "day": day,
                 "pSOL": pSOL_current,
-                "pre_nSOL1": pre_fSOL_SOL_nSOL,
-                "pre_nSOL": pre_fSOL_xSOL_nSOL,
+                "pre_nSOL1": pre_StabilityPoolSOL_values['fSOL_xSOL'][0],
+                "pre_nSOL": pre_StabilityPoolxSOL_values['fSOL_xSOL'][0],
                 "nSOL": state.nSOL,
-                "pre_nF": pre_fSOL_xSOL_nF,
-                "pre_nF1": pre_fSOL_SOL_nF,
+                "pre_nF": pre_StabilityPoolxSOL_values['fSOL_xSOL'][1],
+                "pre_nF1": pre_StabilityPoolSOL_values['fSOL_xSOL'][1],
                 "pF": state.pF,
                 "nF": state.nF,
-                "pre_pX": pre_fSOL_xSOL_pX,
-                "pre_pX1": pre_fSOL_SOL_nX,
+                "pre_pX": pre_StabilityPoolxSOL_values['fSOL_xSOL'][3],
+                "pre_pX1": pre_StabilityPoolSOL_values['fSOL_xSOL'][3],
                 "pX": state.pX,
-                "pre_nX": pre_fSOL_xSOL_nX,
-                "pre_nX1": pre_fSOL_SOL_pX,
+                "pre_nX": pre_StabilityPoolxSOL_values['fSOL_xSOL'][2],
+                "pre_nX1": pre_StabilityPoolSOL_values['fSOL_xSOL'][2],
                 "nX": state.nX,
                 "Marketcap fSOL": state.pF * state.nF,
                 "Marketcap xSOL": state.pX * state.nX,
                 "Collateralization ratio": collateral_ratio,
-                "Collateralization ratio Post Stab1": collateral_ratio_post_1,
-                "Collateralization ratio Post Stab2": collateral_ratio_post_2,
-                "Stab1 nSOL removed": pre_fSOL_xSOL_nSOL - pre_fSOL_SOL_nSOL,
+                "Collateralization ratio Post Stab1": collateral_ratio_post_stab_xSOL,
+                "Collateralization ratio Post Stab2": collateral_ratio_post_stab_SOL,
+                "Stab1 nSOL removed": pre_StabilityPoolxSOL_values['fSOL_xSOL'][0] - post_StabilityPoolxSOL_values['fSOL_xSOL'][0],
                 "Stab1 nF burned": stability_pool_fSOL_xSOL_usage_nF_redeemed,
-                "Stab1 nX minted": pre_fSOL_SOL_nX - pre_fSOL_xSOL_nX,
-                "Stab2 nSOL moved": pre_fSOL_SOL_nSOL - state.nSOL,
+                "Stab1 nX minted":  post_StabilityPoolxSOL_values['fSOL_xSOL'][2] - pre_StabilityPoolxSOL_values['fSOL_xSOL'][2],
+                "Stab2 nSOL moved": pre_StabilityPoolSOL_values['fSOL_xSOL'][0] - post_StabilityPoolSOL_values['fSOL_xSOL'][0],
                 "Stab2 nF burned": stability_pool_fSOL_SOL_usage_nF_redeemed,
-                "Stab2 nX minted": state.nX - pre_fSOL_SOL_nX,
-                "pre_stab_nF": pre_stab_nF,
-                "post_stab_nF": post_stab_nF,
-                "stab_nF": state.stab_nF,
-                "pre_stab2_nF": pre_stab2_nF,
-                "post_stab2_nF": post_stab2_nF,
-                "stab2_nF": state.stab2_nF,
+                "Stab2 nX minted": post_StabilityPoolSOL_values['fSOL_xSOL'][2] - pre_StabilityPoolSOL_values['fSOL_xSOL'][2],
+                "pre_stabSOL_nF": pre_UpdatefSOLInStabilityPool_values['stabSOL_nF'],
+                "post_stabSOL_nF": post_UpdatefSOLInStabilityPool_values['stabSOL_nF'],
+                "stabSOL_nF": state.stabSOL_nF,
+                "pre_stabxSOL_nF": pre_UpdatefSOLInStabilityPool_values['stabxSOL_nF'],
+                "post_stabxSOL_nF": post_UpdatefSOLInStabilityPool_values['stabxSOL_nF'],
+                "stabxSOL_nF": state.stabxSOL_nF,
                 "empty": None
             }
             daily_data.append(day_data)
