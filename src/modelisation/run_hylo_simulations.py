5b94478d1d907d996297592037f663d3a7ea2a17
Shoomsol <154993685+Shoomsol@users.noreply.github.com>
2024-06-19 13:36:58 +0100

    Update run_hylo_simulations.py

--- src/modelisation/run_hylo_simulations.py
+++ src/modelisation/run_hylo_simulations.py
@@ -13,9 +13,8 @@ def run_hylo_simulations(
     sim = Simulation()
 
     for path_index, path in enumerate(price_paths.T):
-        path_results = []
         
         for sub_run_id in range(1, num_runs_per_path + 1):
-            run_result = sim.run_simulation(path, stab_mod_fSOL_SOL, stab_mod_fee_control, stab_mod_fSOL_xSOL, run_id, sub_run_id)
-            path_results.append(run_result) 
+            sim.run_simulation(path, stab_mod_fSOL_SOL, stab_mod_fee_control, stab_mod_fSOL_xSOL, run_id, sub_run_id)
+
             
